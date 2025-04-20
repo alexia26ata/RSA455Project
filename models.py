310b83686ea0c8fdd0cd1ecbf30234969fb47ffc
Alexia Abdel Massih <alexiaabdelmassih@Alexias-MacBook-Pro.local>
2025-04-20 20:50:23 +0300

    Fixed the timezone

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+import pytz
 
 db = SQLAlchemy()
 
@@ -17,6 +18,13 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+def get_lebanon_time():
+    lebanon_tz = pytz.timezone('Asia/Beirut')
+    return datetime.now(lebanon_tz)
+
 class Operation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -25,4 +33,4 @@ class Operation(db.Model):
     output_data = db.Column(db.Text, nullable=False)
     key_size = db.Column(db.Integer, nullable=False)
     keys_used = db.Column(db.Text, nullable=False)  # JSON string of keys used
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow) 
+    timestamp = db.Column(db.DateTime(timezone=True), default=get_lebanon_time) 
